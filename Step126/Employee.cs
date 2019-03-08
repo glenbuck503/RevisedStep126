26197d9b4ad0be0ba865ecdd28ea05af6c32cb2f
Glen Buck <glenbuck503@gmail.com>
2019-03-08 09:44:44 -0800

    finalized 126

---
+++ Step126/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step126
+{
+    public class Employee : Person, IQuittable
+    {
+        public  void SayName()
+        {
+            Console.WriteLine(FirstName + LastName);
+
+        }
+        public void Quit()
+        {
+            Console.WriteLine(FirstName + LastName);
+
+
+            
+            
+        }
+
+    }
+
+}
---
+++ Step126/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee persons = new Employee();
+            persons.FirstName = "Tech";
+            persons.LastName = " Academy";
+            persons.SayName();
+            
+            Console.ReadLine();
+
+            IQuittable employees = new Employee();
+            employees.Quit();
+            Console.ReadLine();
+
+
+
+
+
+
+           
+
+
+        }
+    }
+}
